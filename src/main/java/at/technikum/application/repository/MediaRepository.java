df9204ba07701a9de2becceff8e96e7379d84229
Camila Polzer <157469723+detectivecamila@users.noreply.github.com>
2026-01-06 16:52:46 +0100

    Nicht wichtige Files rauslöschen

--- src/main/java/at/technikum/application/repository/MediaRepository.java
+++
@@ -1,5 +0,0 @@
-package at.technikum.application.repository;
-
-public class MediaRepository {
-    //in liste die daten schreiben
-}
--- src/main/java/at/technikum/application/repository/RatingRepository.java
+++
@@ -1,4 +0,0 @@
-package at.technikum.application.repository;
-
-public class RatingRepository {
-}
